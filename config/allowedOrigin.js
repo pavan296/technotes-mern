e878c6ba0cce6945c19bd305551c7d9269cc8984
Pavan <pavan.b.m@accenture.com>
2022-10-12 15:00:42 +0530

    logs and cors

---
+++ config/allowedOrigin.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://www.dandrepairshop.com',
+  'http://dandrepairshop.com',
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express=require('express');
-const app=express();
-const path=require('path');
-const PORT=process.env.PORT || 3500;
+const express = require('express');
+const app = express();
+const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const PORT = process.env.PORT || 3500;
 
-app.use('/',express.static(path.join(__dirname,'public')))
-app.use(express.json())
-app.use('/',require('./routes/root'))
-app.all('*',(req,res)=>{
-    res.status(404);
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname,'views','404.html'))
-    }else if(req.accepts('json')){
-        res.sendFile({message:'404 Not found'})
-    }else{
-        res.type('txt').send('404 Not found')
-    }
-})
-app.listen(PORT,()=>console.log(`the serve is running on ${PORT}`));
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser);
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.sendFile({ message: '404 Not found' });
+  } else {
+    res.type('txt').send('404 Not found');
+  }
+});
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`the serve is running on ${PORT}`));
