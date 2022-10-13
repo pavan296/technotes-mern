1496808c30dd2e2f4250f9ba4003ce5becc2b914
Pavan <pavan.b.m@accenture.com>
2022-10-13 14:33:45 +0530

    MongoDb connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -5,9 +6,15 @@ const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const { logEvents } = require('./middleware/logger');
+
 const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
 
+console.log(process.env.NODE_ENV);
+connectDB();
 app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -26,4 +33,16 @@ app.all('*', (req, res) => {
   }
 });
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`the serve is running on ${PORT}`));
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`the serve is running on ${PORT}`));
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'MongoErrLog.log'
+  );
+});
